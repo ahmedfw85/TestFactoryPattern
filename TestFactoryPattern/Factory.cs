baba7ef0c7c46a1fa0df687942961fc017309bf1
Ahmad.Wedyan <Ahmad.Wedyan@DPKASTG-05>
2021-03-19 09:28:26 +1300

    Add project files.

---
+++ TestFactoryPattern/Factory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFactoryPattern
+{
+    static class Factory
+    {
+        public static Position Get(int id)
+        {
+            switch (id)
+            {
+                case 0:
+                    return new Manager();
+                case 1:
+                    return new Clerk();
+                default:
+                    return new Programmer();
+            }
+        }
+    }
+}
---
+++ TestFactoryPattern/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestFactoryPattern
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                // Use Factory to get a Position for each id.
+                Position p = Factory.Get(i);
+                // Display the results.
+                label1.Text += "Id = " + i + ", position = " + p.Title + "\r\n";
+            }
+        }
+    }
+}
